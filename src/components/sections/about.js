46859986ce97bd56bbf2e49f24df0c51ad0235d7
IFFranciscoME <if.francisco.me@gmail.com>
2021-05-19 11:33:09 -0500

    personalization 2

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -164,7 +164,7 @@ const About = () => {
           <div className="wrapper">
             <StaticImage
               className="img"
-              src="../../images/me.jpeg"
+              src="../../images/me.jpg"
               width={500}
               quality={95}
               formats={['AUTO', 'WEBP', 'AVIF']}
