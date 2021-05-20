6195b95c2bb72f1d865f96f9e394a061375ff0d2
IFFranciscoME <if.francisco.me@gmail.com>
2021-05-20 10:03:15 -0500

    personalization 5

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
-import { Layout, Hero, About, Featured, Awards, Jobs, Projects, Contact } from '@components';
+import { Layout, Hero, About, Featured, Awards, Projects, Jobs, Contact } from '@components';
 
 const StyledMainContainer = styled.main`
   counter-reset: section;
@@ -14,8 +14,8 @@ const IndexPage = ({ location }) => (
       <About />
       <Featured />
       <Awards />
-      <Jobs />
       <Projects />
+      <Jobs />
       <Contact />
     </StyledMainContainer>
   </Layout>
