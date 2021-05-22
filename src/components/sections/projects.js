0657000783509c7a7dbb0017f44427c5b4bba828
IFFranciscoME <if.francisco.me@gmail.com>
2021-05-22 16:08:32 -0500

    projects grid

--- src/components/sections/projects.js
+++ src/components/sections/projects.js
@@ -27,13 +27,14 @@ const StyledProjectsSection = styled.section`
   .projects-grid {
     ${({ theme }) => theme.mixins.resetList};
     display: grid;
-    grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
+    max-width: 1080px;
+    grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
     grid-gap: 15px;
     position: relative;
     margin-top: 50px;
 
     @media (max-width: 1080px) {
-      grid-template-columns: repeat(auto-fill, minmax(800px, 1fr));
+      grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
     }
   }
 
